ef7cde8070723ae6b1db3329b4d1e901cbbc3345
mattawee <59027419+mattawee@users.noreply.github.com>
2019-12-19 01:59:38 +0700

    Add files via upload

---
+++ Banana.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ome_a
+ */
+public class Banana extends fruit{
+    private String Pack ;
+    
+}
